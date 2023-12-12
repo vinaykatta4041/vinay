d801c37b69d6ba854320f5811127a05491ffde10
vinaykatta4041 <153638238+vinaykatta4041@users.noreply.github.com>
2023-12-12 22:10:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var score=0;
+function Decrease(){
+    score=score-10;
+    document.getElementById("box").textContent=score;
+}
+function Increase(){
+     score=score+10;
+    document.getElementById("box").textContent=score;
+}
